4eb96c31472b1e0c3ec4f9fc4ab8233592d2e772
Travis James <registerfault@gmail.com>
2020-08-05 15:15:49 -0400

    rename HDC MSRs and adjust consumers

--- src/msr.h
+++ src/msr.h
@@ -184,23 +184,32 @@ typedef union {
         uint64_t w;
 } IA32_HWP_REQUEST;
 
-#define PKG_HDC_CTL_MSR 0x00000DB0
+#define IA32_PKG_HDC_CTL_MSR 0x00000DB0
 typedef union {
         struct {
                 uint8_t enable:1;
                 uint64_t res:63;
         } s __attribute__((packed));
         uint64_t w;
-} PKG_HDC_CTL;
+} IA32_PKG_HDC_CTL;
 
-#define PM_CTL1_MSR 0x00000652
+#define IA32_PKG_HDC_CONFIG_CTL_MSR 0x00000652
+typedef union {
+        struct {
+                uint8_t threshold:3;
+                uint64_t res:64-3;
+        } s __attribute__((packed));
+        uint64_t w;
+} IA32_PKG_HDC_CONFIG_CTL;
+
+#define IA32_PM_CTL1_MSR 0x00000DB1
 typedef union {
         struct {
                 uint8_t allow:1;
                 uint64_t res:63;
         } s __attribute__((packed));
         uint64_t w;
-} PM_CTL1;
+} IA32_PM_CTL1;
 
 #define CPU_PLANE 0
 #define GPU_PLANE 1
--- src/test.c
+++ src/test.c
@@ -3,19 +3,22 @@
 
 int main()
 {
-        PKG_HDC_CTL r;
-        PM_CTL1 p;
+        IA32_PKG_HDC_CTL r;
+        IA32_PM_CTL1 p;
 
         p.w = 0;
         p.s.allow = 1;
-        wrmsr_all(PM_CTL1_MSR, p.w);
+        r.w = 0;
+        r.s.enable = 1;
+        wrmsr(IA32_PKG_HDC_CTL_MSR, r.w);
+        wrmsr_all(IA32_PM_CTL1_MSR, p.w);
 
-        r.w = rdmsr(PKG_HDC_CTL_MSR);
-        p.w = rdmsr(PM_CTL1_MSR);
-        printf("PKG_HDC_CTL\n");
+        r.w = rdmsr(IA32_PKG_HDC_CTL_MSR);
+        p.w = rdmsr(IA32_PM_CTL1_MSR);
+        printf("IA32_PKG_HDC_CTL\n");
         printf("enable: %lx\n", r.s.enable);
         printf("res: %lx\n", r.s.res);
-        printf("PM_CTL1_MSR\n");
+        printf("IA32_PM_CTL1_MSR\n");
         printf("enable: %lx\n", p.s.allow);
         printf("res: %lx\n", p.s.res);
         return 0;
